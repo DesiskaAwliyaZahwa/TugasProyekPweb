8cc944562b462da33e979f40964fb4e6c8d29950
Desiska Awliya Zahwa <62451744+DesiskaAwliyaZahwa@users.noreply.github.com>
2020-07-27 12:00:58 +0700

    Update upload.php

--- upload_seoul/upload.php
+++ upload_seoul/upload.php
@@ -23,7 +23,7 @@
         $sql = mysqli_query($connect, $query); // Eksekusi/ Jalankan query dari variabel $query 
         if($sql){ // Cek jika proses simpan ke database sukses atau tidak
           // Jika Sukses, Lakukan :
-          header("location: seoul.php"); // Redirectke halaman index.php
+          header("location: seoul.php"); // Redirectke halaman seoul.php
         }
         else{
           // Jika Gagal, Lakukan :
